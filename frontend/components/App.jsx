2d87ba1a4c7e349ee9b31cd343952e95644405aa
pts9789 <pts9789@aol.com>
2017-10-24 15:33:59 -0400

    Add App and root components

---
+++ frontend/components/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <h1>SRO(ttenTomatoes)</h1>
+  </div>
+);
+
+export default App;
--- frontend/sro.jsx
+++ frontend/sro.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to SRO(ttenTomatoes)</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
