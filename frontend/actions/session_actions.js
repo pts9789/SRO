01537607036da6dc60370d81212529dd10adb967
pts9789 <pts9789@aol.com>
2017-10-25 11:13:48 -0400

    Fix session_ errors reducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,28 +1,32 @@
 import * as ApiUtil from "../util/session_api_util";
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
-export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 export const receiveCurrentUser = (currentuser) => ({
   type: RECEIVE_CURRENT_USER,
   currentuser
 });
 
-export const receiveErrors = (errors) => ({
-  type: RECEIVE_ERRORS,
+export const receiveSessionErrors = (errors) => ({
+  type: RECEIVE_SESSION_ERRORS,
   errors
 });
 
 export const login = (user) => (dispatch) => {
   return ApiUtil.login(user).then((user) => {
     return dispatch(receiveCurrentUser(user));
-  });
+  }, (err) => (
+    dispatch(receiveSessionErrors(err.responseJSON))
+  ));
 };
 
 export const signup = (user) => (dispatch) => {
   return ApiUtil.signup(user).then((user) => {
     return dispatch(receiveCurrentUser(user));
-  });
+  }, (err) => (
+    dispatch(receiveSessioonErrors(err.responseJSON))
+  ));
 };
 
 export const logout = () => (dispatch) => {
