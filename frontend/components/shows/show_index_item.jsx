20ca32bfc46e60d2a7db916f6772997776ed1a2b
pts9789 <pts9789@aol.com>
2017-10-30 14:24:11 -0400

    Remove underline from show link

--- frontend/components/shows/show_index_item.jsx
+++ frontend/components/shows/show_index_item.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
 
 const ShowIndexItem = ({ show, router, history }) => (
-  <li>
-    <Link to={`/shows/${show.id}`}>{show.title}</Link>
+  <li className="show-index-item">
+    <Link to={`/shows/${show.id}`} className="link-to-show" >{show.title}</Link>
   </li>
 );
 
