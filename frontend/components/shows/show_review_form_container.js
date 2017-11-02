634ad29fe92268a942b714358aaee327e97f2350
pts9789 <pts9789@aol.com>
2017-11-01 22:27:16 -0400

    Add show_review_form_container

--- frontend/components/shows/show_review_form_container.js
+++ frontend/components/shows/show_review_form_container.js
@@ -1,10 +1,10 @@
 import { connect } from 'react-redux';
-
-// import { createReview } from '../../actions/shows_actions.js';
-// DON'T FORGET TO WRITE YOUR REVIEW REDUCER ACTIONS AND EVERYTHING!!!
+import { createReview } from '../../actions/review_actions';
 import ShowReviewForm from './review_form';
+// DON'T FORGET TO WRITE YOUR REVIEW REDUCER ACTIONS AND EVERYTHING!!!
 
 const mapDispatchToProps = dispatch => ({
+  createReview: (review) => dispatch(createReview(review)),
 });
 
 export default connect(null, mapDispatchToProps)(ShowReviewForm);
