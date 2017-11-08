9ff99b0e4ee5cfb88de6b0576aa8f0ea10aa07ca
pts9789 <pts9789@aol.com>
2017-11-08 15:21:43 -0500

    Clear errors when stiching modals

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,5 +1,5 @@
 import { RECEIVE_SESSION_ERRORS, RECEIVE_CURRENT_USER} from '../actions/session_actions';
-import { HIDE_MODAL } from '../actions/modal_actions';
+import { HIDE_MODAL, SHOW_MODAL } from '../actions/modal_actions';
 
 const SessionErrorsReducer = (state = [], action) => {
   Object.freeze(state);
@@ -8,6 +8,8 @@ const SessionErrorsReducer = (state = [], action) => {
     case RECEIVE_SESSION_ERRORS:
       return action.errors;
     case HIDE_MODAL:
+      return [];
+    case SHOW_MODAL:
       return [];
     case RECEIVE_CURRENT_USER:
       return [];
