f737e892718d1bc6af4e53286e4e0c6128384713
pts9789 <pts9789@aol.com>
2018-01-12 18:07:12 -0500

    Format Footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Footer = () => (
   <div className="footer-container">
     <div className="footer-main">
+
       <p className="footer-text">&copy; Standing Room Only</p>
 
     </div>
