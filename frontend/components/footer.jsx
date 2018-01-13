bc9e3a186095a04db39a1f9e1375f71475a761bd
pts9789 <pts9789@aol.com>
2018-01-12 19:00:18 -0500

    Add contact info to footer

--- frontend/components/footer.jsx
+++ frontend/components/footer.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 const Footer = () => (
   <div className="footer-container">
-    <div className="footer-main">
+    <ul className="footer-main">
+      <a href="mailto:pspencer.ny@gmail.com" className="contact-text" >Contact</a>
+      <a href="http://github.com/pts9789" className="contact-text" target="_blank">GitHub</a>
+      <a href="https://www.linkedin.com/in/patrick-spencer-950645152/" className="contact-text" target="_blank">LinkedIn</a>
+      <a href="http://www.patrick-spencer.nyc/" className="contact-text" target="_blank">Portfolio</a>
 
-      <p className="footer-text">&copy; Standing Room Only</p>
-
-    </div>
+    </ul>
+    <p className="footer-text">&copy; Standing Room Only</p>
   </div>
 );
 
